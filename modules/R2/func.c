962b8c04113f253f7bc086b392fa10dd65d46467
Mubarak <ma0192@mix.wvu.edu>
2021-09-19 22:15:05 -0400

    added findPCB() function

--- modules/R2/func.c
+++ modules/R2/func.c
@@ -5,38 +5,94 @@
 
 
 // where all the functions of R2 would be (along with itoa())
+//also where all the queues are initialized for function use
+
+
+struct queue ready;					// initializing the ready queue
+ready.head = NULL;
+ready.tail = NULL;
+
+struct queue blocked;				// initializing the blocked queue
+blocked.head = NULL;
+blocked.tail = NULL;
+
+struct queue sus_ready;				// initializing the suspended ready queue
+sus_ready.head = NULL;
+sus_ready.tail = NULL;
+
+struct queue sus_blocked;			// initializing the suspended blocked queue
+sus_blocked.head = NULL;
+sus_blocked.tail = NULL;
 
 
 
 /*
 FindPCB() is given a name of a PCB. Searches all queues until it finds a match.
-When it does, returns a pointer to the PCB with the same name
+When it does, returns a pointer to the PCB of the same name. If no match is found, return NULL
 */
-// pcb* FindPCB(char* name) {
-	// make a temp pointer
-	// loop through all queues (each queue in a separate loop)
-	// compare each PCB's name with the given name (using strcmp(name, "PCB"->name) == 0 )
-	// if a match is found, get the temp pointer to it
-	// return the pointer
-	// if no match, return NULL
+pcb* FindPCB(char* name) {
 	
-	// /*
-	// if (match)
-		// return temp pointer;
+	pcb* ptr = ready.head;					// the indexing pointer (used for traversing each queue, starting with ready queue)
 	
-	// else
-		// return NULL;
+	while (ptr != NULL) {					// loop through ready queue
+		
+		if (strcmp(name, ptr->name) == 0) 	// if given name matches current PCB's name
+			return ptr;						// return the pointer (i.e PCB found)
+		
+		
+		ptr = ptr->next;					// go to next PCB in queue
+	}
 	
 	
-	// */
+	// not found in ready, try in blocked
+	
+	ptr = blocked.head;						
+	
+	while (ptr != NULL) {					// loop through blocked queue
+		
+		if (strcmp(name, ptr->name) == 0) 	// if given name matches current PCB's name
+			return ptr;						// return the pointer (i.e PCB found)
+		
+		
+		ptr = ptr->next;					// go to next PCB in queue
+	}
 	
 	
+	// not found in blocked, try in suspended ready
 	
-// }
+	pcb* ptr = sus_ready.head;				// the indexing pointer
+	
+	while (ptr != NULL) {					// loop through sus_ready queue
+		
+		if (strcmp(name, ptr->name) == 0) 	// if given name matches current PCB's name
+			return ptr;						// return the pointer (i.e PCB found)
+		
+		
+		ptr = ptr->next;					// go to next PCB in queue
+	}
+	
+	
+	// not found in suspended ready, try in suspended blocked
+	
+	pcb* ptr = sus_blocked.head;				// the indexing pointer
+	
+	while (ptr != NULL) {					// loop through sus_ready queue
+		
+		if (strcmp(name, ptr->name) == 0) 	// if given name matches current PCB's name
+			return ptr;						// return the pointer (i.e PCB found)
+		
+		
+		ptr = ptr->next;					// go to next PCB in queue
+	}
+	
+	
+	// not found in any queue, then PCB doesn't exist, return null
+	return NULL;
+}
 
 
 pcb* allocatePCB() {
-	pcb* alloc = (pcb*)sys_alloc_mem(sizeof(pcb)); // initialized a pcb pointer, allowing pointer to point at a specific location of memory (memory is the zie of pcb)
+	pcb* alloc = (pcb*)sys_alloc_mem(sizeof(pcb)); // initialized a pcb pointer, allowing pointer to point at a specific location of memory (memory is the size of pcb)
 	alloc->prio=5;
 	alloc->susp=0;
 	return alloc;
@@ -44,90 +100,12 @@ pcb* allocatePCB() {
 
 
 
-
-
-
-// itoa() and helper functions below
-
-// used to swap two given values with each other (useful for arrays) using pointers to chars
-void swap(char *a, char *b) {
-	
-	char temp = *a; 	// temporarily store first value to avoid losing it
-	*a = *b;			// store the second value in the first position
-	*b = temp;			// store the first value in the seocnd position
-}
-
-
-// used to flip a given string array (first charatcter becomes last, etc) using swap() finction in a loop
-// takes the string to be flipped, and the string's length
-char* flip(char *str, int length) {
-	
-	int i = 0, j = length-1;	// two indices at beginning and ending of string
-	
-	while (i < j) {				// while indices did not cross each other yet
-		
-		swap(&str[i], &str[j]);	// swap the characters where the indices are at, and move each index one step to the middle
-		i = i+1;
-		j = j-1;
-		
-		
-	}
-	return str;
-	
-}
-
-
-// used to convert an int value to a string (while keeping its value) assuming its base 10
-char* itoa(int number, char *str ) {
-	
-	int isNegative;					// flag used to determine if given number is negative or not
-	int temp;						// this will store the absolute value of given number
-	int i = 0;						// index for given string
-	
-	if (number < 0) {
-		isNegative = 1;
-		temp = number * -1;
-
-	}
-		
-	else {
-		isNegative = 0;
-		temp = number;
-	}
-	
-	
-	while (temp) {
-		
-		int j = temp % 10;
-		str[i] = 48+j;			// 48 is the ASCII for '0'
-		i = i+1;
-		temp = temp/10;
-
-		
-	}
-	
-	if (i == 0) {				// if given number is 0, i would not move and remain at 0 (while loop doesn't execute)
-		
-		str[i] = '0';
-		i = i+1;
-		
-	}
-	
-	if (isNegative) {			// add the minus sign to the string if it was negative
-		
-		str[i] = '-';
-		i = i+1;
-	}
-	
-	str[i] = '\0';				// end the string
-	
-	return flip(str,strlen(str));
-	
-}
 char* FreePCB( pcb* name ){
 
 return sys_free_mem(name );// return free memmory function
 }// free pcb end 
+
+
 
 void InsertPCB( pcb* pntr) {
 
@@ -207,3 +185,83 @@ int removePCB (pcb* pntr) {
 }
 
 
+//==============================================================================================================
+
+
+// itoa() and helper functions below
+
+// used to swap two given values with each other (useful for arrays) using pointers to chars
+void swap(char *a, char *b) {
+	
+	char temp = *a; 	// temporarily store first value to avoid losing it
+	*a = *b;			// store the second value in the first position
+	*b = temp;			// store the first value in the seocnd position
+}
+
+
+// used to flip a given string array (first charatcter becomes last, etc) using swap() finction in a loop
+// takes the string to be flipped, and the string's length
+char* flip(char *str, int length) {
+	
+	int i = 0, j = length-1;	// two indices at beginning and ending of string
+	
+	while (i < j) {				// while indices did not cross each other yet
+		
+		swap(&str[i], &str[j]);	// swap the characters where the indices are at, and move each index one step to the middle
+		i = i+1;
+		j = j-1;
+		
+		
+	}
+	return str;
+	
+}
+
+
+// used to convert an int value to a string (while keeping its value) assuming its base 10
+char* itoa(int number, char *str ) {
+	
+	int isNegative;					// flag used to determine if given number is negative or not
+	int temp;						// this will store the absolute value of given number
+	int i = 0;						// index for given string
+	
+	if (number < 0) {
+		isNegative = 1;
+		temp = number * -1;
+
+	}
+		
+	else {
+		isNegative = 0;
+		temp = number;
+	}
+	
+	
+	while (temp) {
+		
+		int j = temp % 10;
+		str[i] = 48+j;			// 48 is the ASCII for '0'
+		i = i+1;
+		temp = temp/10;
+
+		
+	}
+	
+	if (i == 0) {				// if given number is 0, i would not move and remain at 0 (while loop doesn't execute)
+		
+		str[i] = '0';
+		i = i+1;
+		
+	}
+	
+	if (isNegative) {			// add the minus sign to the string if it was negative
+		
+		str[i] = '-';
+		i = i+1;
+	}
+	
+	str[i] = '\0';				// end the string
+	
+	return flip(str,strlen(str));
+	
+}
