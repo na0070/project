78ddd3ec20efb2ece18eb3e705303abbd19e555e
Mubxrak <70289332+Mubxrak@users.noreply.github.com>
2021-10-22 00:32:29 -0400

    Update help.c

--- modules/R1/help.c
+++ modules/R1/help.c
@@ -11,11 +11,12 @@
 @param none
 */
 void help(){
-print("commands: [version] [settime] [gettime] [setdate] [getdate] [shutdown] [create PCB] [delete PCB] [block PCB] [unblock PCB] \n[suspend PCB] [resume PCB] [set PCB priority] [show PCB] [show ready] [show blocked] [show all] [clear] [help] \n",151);
-
+print("commands: [version] [settime] [gettime] [setdate] [getdate] [shutdown] [delete PCB] [block PCB] [unblock PCB] \n[suspend PCB] [resume PCB] [set PCB priority] [show PCB] [show ready] [show blocked] [show all] [clear] [help] \n",151);
+print("[loadr3] [alarm] \n",20);
+  
 print("setdate [setdate year month day] \n",35);
 print("settime [settime hour minute second] \n",39);
-print("pcb create (name) (class) (#priority) \n",39);
+//print("pcb create (name) (class) (#priority) \n",39);
 print("pcb delete (name) \n",20);
 print("pcb block (name) \n",19);
 print("pcb unblock (name) \n",21);
@@ -26,6 +27,9 @@ print("pcb show (name) \n",19);
 print("pcb show ready \n",19);
 print("pcb show blocked \n",19);
 print("pcb show all \n",15);
+print("loadr3 \n",9);
+print("alarm (message) (delay) \n",26);
+
 
 
 }
