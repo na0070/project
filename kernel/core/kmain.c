2b5099460efa508f51f274ee4640e3a5d68f286f
Mubxrak <70289332+Mubxrak@users.noreply.github.com>
2021-10-22 00:39:42 -0400

    Update kmain.c

--- kernel/core/kmain.c
+++ kernel/core/kmain.c
@@ -50,7 +50,7 @@ void kmain(void)
    //     MPX Module.  This will change with each module.
    // you will need to call mpx_init from the mpx_supt.c
    
-   mpx_init(MODULE_R3);
+   mpx_init(MODULE_R4);
  	
    // 2) Check that the boot was successful and correct when using grub
    // Comment this when booting the kernel directly using QEMU, etc.
