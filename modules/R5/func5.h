2a6ccd01442d941e45b8a94802844f69d481f2ac
asselalabbadi <aa0316@mix.wvu.edu>
2021-10-25 20:29:15 -0400

    R5 structs

--- modules/R5/func5.h
+++ modules/R5/func5.h
@@ -6,4 +6,21 @@
 #define FALSE 0
 
 // CMCB and LMCB structs should go here
+typedef struct cmcb{
+ char *Type; 
+ char *beginning_address;
+ int *Size;
+ char *name;  
+ CMBC next*;
+}  CMCB;
+
+typedef struct lmcb{
+ char *Type; 
+ int *Size;
+}  LMCB;
+
 // function prototypes should go here
+struct list {
+        CMCB *head; // dequeue this 
+	CMCB *tail; // enquue affter this 
+}
