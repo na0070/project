fdf7c189cafa4fcf9e08f187b93ea2f817c6834a
Mubarak <mubarak@Mubaraks-MacBook-Air.local>
2021-11-08 15:30:50 -0500

    .

--- modules/R6/func6.h
+++ modules/R6/func6.h
@@ -2,20 +2,31 @@
 #include "../R2/func.h"
 #include "../mpx_supt.h"
 
+
+// DCB struct should go here
+
+
+
+
+
+
+
+
+
+
+
+
+
 // IOCB struct
-
 typedef struct iocb {
-
-pcb* process;       // the process requesting IO
-DCB* device;	    // device to be used for IO
-int op;		        // the operation code (op_code)
-char* buffer;       // the IOCB’s buffer (from sys_req)
-int* count;	        // counter to whats in the buffer (from sys_req)
-
+    pcb* process;       // the process requesting IO
+    DCB* device;	    // device to be used for IO
+    int op;		        // the operation code (op_code)
+    char* buffer;       // the IOCB’s buffer (from sys_req)
+    int* count;	        // counter to whats in the buffer (from sys_req)
 } IOCB;
 
 
-
-// function prototypes
+// function prototypes go here
 
 int com_write (char* buf_p, int* count_p);
