40cdeacca88f6a2005397967f37e4a8ff8fed7bf
Mubarak <mubarak@Mubaraks-MacBook-Air.local>
2021-11-08 14:02:39 -0500

    initial R6 work

---
+++ modules/R6/func6.c
@@ -0,0 +1,16 @@
+// functions for R6
+#include "func6.h"
+
+// com_write function
+
+int com_write (char* buf_p, int* count_p) {
+
+// get first character from requestor’s buffer
+// store to output buffer
+// enable the ‘write’ interrupt
+// ‘write’ interrupt handler performs rest
+
+// note: sys_req WRITE also uses buffer pointer and count pointer
+
+    return 0;
+}
---
+++ modules/R6/func6.h
@@ -0,0 +1,21 @@
+// header for func6.c
+#include "../R2/func.h"
+#include "../mpx_supt.h"
+
+// IOCB struct
+
+typedef struct iocb {
+
+pcb* process;       // the process requesting IO
+DCB* device;	    // device to be used for IO
+int op;		        // the operation code (op_code)
+char* buffer;       // the IOCB’s buffer (from sys_req)
+int* count;	        // counter to whats in the buffer (from sys_req)
+
+} IOCB;
+
+
+
+// function prototypes
+
+int com_write (char* buf_p, int* count_p);
